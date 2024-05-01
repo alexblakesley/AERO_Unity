98822b37c1ff5ad8f51a18eef373ad85e7a3fe8f
Alex Blakesley <alex.blakesley@loanpad.com>
2024-05-01 17:41:22 +0100

    Minor debugging fix

--- Assets/Drone/FixedWingDynamicsAP.cs
+++ Assets/Drone/FixedWingDynamicsAP.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using System;
 
 public class FixedWingDynamicsAP
 {
